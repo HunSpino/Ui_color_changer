1b7432671c331144fcc5ee43e38f694f4f9ca049
Tamas-Levente <tamasleventelaszlo99@gmail.com>
2021-11-09 17:12:39 +0100

    fügvények megvannak+pár változás

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+function szoveg(){
+    let szoveg = document.getElementById("szovegInput").value;
+    if (szoveg == "" || !szoveg.trim()) {
+        document.getElementById("szovegError").innerHTML = "A mintaszöveg nem lehet üres!";
+        document.getElementById("Output").innerHTML = "";
+    }
+    else {
+        document.getElementById("Output").innerHTML = szoveg;
+        document.getElementById("szovegError").innerHTML = "";
+    }
+}
 
+function szovegMeret(){
+    let meret=document.getElementById("meretInput").value;
+    document.getElementById("Output").style.fontSize = meretInput+"pt";
+}
+function szovegSzin(){
+    let betuszin=document.getElementById("colorSzovegInput").value;
+    document.getElementById("Output").style.color=colorSzovegInput;
+}
+function hatterSzin(){
+    let betuszin=document.getElementById("colorHatterInput").value;
+    document.getElementById("Output").style.backgroundColor=colorHatterInput;
+}
 
 function init(){
-
+    document.getElementById("szovegInput").addEventListener("input", szoveg);
+    document.getElementById("meretInput").addEventListener("input", szovegMeret);
+    document.getElementById("colorSzovegInput").addEventListener("input", szovegSzin);
+    document.getElementById("colorHatterInput").addEventListener("input", hatterSzin);
 }
 
 document.addEventListener('DOMContentLoaded', init);
